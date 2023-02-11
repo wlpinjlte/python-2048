99274bc706b667481e62d0f81586f411f1d577b2
admin-Komputer\admin <wagulec123@gmail.com>
2023-02-11 14:41:35 +0100

    simple gui with key events

--- main.py
+++ main.py
@@ -1,116 +1,178 @@
 import random
+import time
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
 
-def updateFiledToFill(board,freeField):
-    fieldToChoose=random.randint(0,len(freeField)-1)
-    board[freeField[fieldToChoose][0]][freeField[fieldToChoose][1]]=2
+# Game BackEnd
+class Game:
+    def __init__(self):
+        self.board=[[0 for i in range(4)]for j in range(4)]
+        self.board[1][2]=2
+        self.board[2][1]=2
 
-def endGame():
-    pass
+    def getBoard(self):
+        return self.board
 
-def move(board,direction):
-    isMarge=[[False for i in range(len(board[0]))]for j in range(len(board))]
-    maxX=len(board)-1
-    maxY=len(board)-1
-    if(direction=='left'):
-        for x in range(len(board)):
-            firstIteam=0
-            positonToPlaceIteam=0
-            while firstIteam<len(board[0]) and board[x][firstIteam]==0:
-                firstIteam+=1
-            if firstIteam==len(board):
-                continue
-            for i in range(len(board)-firstIteam):
-                if(board[x][firstIteam + i]):
+    def updateFiledToFill(self):
+        freeField = []
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == 0:
+                    freeField.append((i, j))
+
+        if len(freeField) == 0:
+            return
+
+        fieldToChoose = random.randint(0, len(freeField) - 1)
+        self.board[freeField[fieldToChoose][0]][freeField[fieldToChoose][1]] = 2
+
+    def endGame(self):
+        pass
+
+    def move(self, direction):
+        isMarge = [[False for i in range(len(self.board[0]))] for j in range(len(self.board))]
+        maxX = len(self.board) - 1
+        maxY = len(self.board) - 1
+        if (direction == 'left'):
+            for x in range(len(self.board)):
+                firstIteam = 0
+                positonToPlaceIteam = 0
+                while firstIteam < len(self.board[0]) and self.board[x][firstIteam] == 0:
+                    firstIteam += 1
+                if firstIteam == len(self.board):
                     continue
-                temp = board[x][firstIteam + i]
-                board[x][firstIteam + i] = 0
-                board[x][positonToPlaceIteam]=temp
+                for i in range(len(self.board) - firstIteam):
+                    if (self.board[x][firstIteam + i] == 0):
+                        continue
+                    temp = self.board[x][firstIteam + i]
+                    self.board[x][firstIteam + i] = 0
+                    self.board[x][positonToPlaceIteam] = temp
 
-                if positonToPlaceIteam>0 and isMarge[x][positonToPlaceIteam-1]==False and board[x][positonToPlaceIteam-1]==board[x][positonToPlaceIteam]:
-                    board[x][positonToPlaceIteam-1]=board[x][positonToPlaceIteam-1]*2
-                    board[x][positonToPlaceIteam]=0
-                    isMarge[x][positonToPlaceIteam - 1]=True
-                else:
-                    positonToPlaceIteam+=1
+                    if positonToPlaceIteam > 0 and isMarge[x][positonToPlaceIteam - 1] == False and self.board[x][
+                        positonToPlaceIteam - 1] == self.board[x][positonToPlaceIteam]:
+                        self.board[x][positonToPlaceIteam - 1] = self.board[x][positonToPlaceIteam - 1] * 2
+                        self.board[x][positonToPlaceIteam] = 0
+                        isMarge[x][positonToPlaceIteam - 1] = True
+                    else:
+                        positonToPlaceIteam += 1
 
-    elif(direction=='right'):
-        for x in range(len(board)):
-            firstIteam=maxY
-            positonToPlaceIteam=maxY
-            while firstIteam>-1 and board[x][firstIteam]==0:
-                firstIteam-=1
-            if firstIteam==-1:
-                continue
-            for i in range(firstIteam+1):
-                if(board[x][firstIteam - i]==0):
+        elif (direction == 'right'):
+            for x in range(len(self.board)):
+                firstIteam = maxY
+                positonToPlaceIteam = maxY
+                while firstIteam > -1 and self.board[x][firstIteam] == 0:
+                    firstIteam -= 1
+                if firstIteam == -1:
                     continue
-                temp = board[x][firstIteam - i]
-                board[x][firstIteam - i] = 0
-                board[x][positonToPlaceIteam]=temp
+                for i in range(firstIteam + 1):
+                    if (self.board[x][firstIteam - i] == 0):
+                        continue
+                    temp = self.board[x][firstIteam - i]
+                    self.board[x][firstIteam - i] = 0
+                    self.board[x][positonToPlaceIteam] = temp
 
-                if positonToPlaceIteam<maxY and isMarge[x][positonToPlaceIteam+1]==False and board[x][positonToPlaceIteam+1]==board[x][positonToPlaceIteam]:
-                    board[x][positonToPlaceIteam+1]=board[x][positonToPlaceIteam+1]*2
-                    board[x][positonToPlaceIteam]=0
-                    isMarge[x][positonToPlaceIteam + 1]=True
-                else:
-                    positonToPlaceIteam-=1
-    elif (direction == 'up'):
-        for y in range(len(board)):
-            firstIteam=0
-            positonToPlaceIteam=0
-            while firstIteam<len(board) and board[firstIteam][y]==0:
-                firstIteam+=1
-            if firstIteam==len(board):
-                continue
-            for i in range(len(board)-firstIteam):
-                if(board[firstIteam + i][y]==0):
+                    if positonToPlaceIteam < maxY and isMarge[x][positonToPlaceIteam + 1] == False and self.board[x][
+                        positonToPlaceIteam + 1] == self.board[x][positonToPlaceIteam]:
+                        self.board[x][positonToPlaceIteam + 1] = self.board[x][positonToPlaceIteam + 1] * 2
+                        self.board[x][positonToPlaceIteam] = 0
+                        isMarge[x][positonToPlaceIteam + 1] = True
+                    else:
+                        positonToPlaceIteam -= 1
+        elif (direction == 'up'):
+            for y in range(len(self.board)):
+                firstIteam = 0
+                positonToPlaceIteam = 0
+                while firstIteam < len(self.board) and self.board[firstIteam][y] == 0:
+                    firstIteam += 1
+                if firstIteam == len(self.board):
                     continue
-                temp = board[firstIteam + i][y]
-                board[firstIteam + i][y] = 0
-                board[positonToPlaceIteam][y]=temp
+                for i in range(len(self.board) - firstIteam):
+                    if (self.board[firstIteam + i][y] == 0):
+                        continue
+                    temp = self.board[firstIteam + i][y]
+                    self.board[firstIteam + i][y] = 0
+                    self.board[positonToPlaceIteam][y] = temp
 
-                if positonToPlaceIteam>0 and isMarge[positonToPlaceIteam-1][y]==False and board[positonToPlaceIteam-1][y]==board[positonToPlaceIteam][y]:
-                    board[positonToPlaceIteam-1][y]=board[positonToPlaceIteam-1][y]*2
-                    board[positonToPlaceIteam][y]=0
-                    isMarge[positonToPlaceIteam - 1][y]=True
-                else:
-                    positonToPlaceIteam+=1
-    elif (direction == 'down'):
-        for y in range(len(board)):
-            firstIteam=maxX
-            positonToPlaceIteam=maxX
-            while firstIteam>-1 and board[firstIteam][y]==0:
-                firstIteam-=1
-            if firstIteam==-1:
-                continue
-            for i in range(firstIteam+1):
-                if(board[firstIteam - i][y]==0):
+                    if positonToPlaceIteam > 0 and isMarge[positonToPlaceIteam - 1][y] == False and \
+                            self.board[positonToPlaceIteam - 1][y] == self.board[positonToPlaceIteam][y]:
+                        self.board[positonToPlaceIteam - 1][y] = self.board[positonToPlaceIteam - 1][y] * 2
+                        self.board[positonToPlaceIteam][y] = 0
+                        isMarge[positonToPlaceIteam - 1][y] = True
+                    else:
+                        positonToPlaceIteam += 1
+        elif (direction == 'down'):
+            for y in range(len(self.board)):
+                firstIteam = maxX
+                positonToPlaceIteam = maxX
+                while firstIteam > -1 and self.board[firstIteam][y] == 0:
+                    firstIteam -= 1
+                if firstIteam == -1:
                     continue
-                temp = board[firstIteam - i][y]
-                board[firstIteam - i][y] = 0
-                board[positonToPlaceIteam][y]=temp
+                for i in range(firstIteam + 1):
+                    if (self.board[firstIteam - i][y] == 0):
+                        continue
+                    temp = self.board[firstIteam - i][y]
+                    self.board[firstIteam - i][y] = 0
+                    self.board[positonToPlaceIteam][y] = temp
 
-                if positonToPlaceIteam<maxY and isMarge[positonToPlaceIteam+1][y]==False and board[positonToPlaceIteam+1][y]==board[positonToPlaceIteam][y]:
-                    board[positonToPlaceIteam+1][y]=board[positonToPlaceIteam+1][y]*2
-                    board[positonToPlaceIteam][y]=0
-                    isMarge[positonToPlaceIteam + 1][y]=True
-                else:
-                    positonToPlaceIteam-=1
+                    if positonToPlaceIteam < maxY and isMarge[positonToPlaceIteam + 1][y] == False and \
+                            self.board[positonToPlaceIteam + 1][y] == self.board[positonToPlaceIteam][y]:
+                        self.board[positonToPlaceIteam + 1][y] = self.board[positonToPlaceIteam + 1][y] * 2
+                        self.board[positonToPlaceIteam][y] = 0
+                        isMarge[positonToPlaceIteam + 1][y] = True
+                    else:
+                        positonToPlaceIteam -= 1
 
-def tempPrint(board):
-    for i in board:
-        for j in i:
-            print(j,end=" ")
-        print()
 
-board=[[0 for j in range(4)]for i in range(4)]
-board[0][0]=2
-board[0][1]=2
-board[0][3]=2
-board[0][2]=2
-board[3][0]=2
-board[1][0]=2
-board[2][0]=2
-print(tempPrint(board))
-move(board,"up")
-print(tempPrint(board))
+# Game FrontEnd(Gui)
+
+class Template(Widget):
+    mainGrid = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.game=Game()
+        board = self.game.getBoard()
+
+        self._keyboard = Window.request_keyboard(
+            self._keyboard_closed, self, 'text')
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+        self.guiBoard = GridLayout(cols=4)
+        self.fields = [[None for i in range(len(board[0]))] for j in range(len(board))]
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                image = Label(text=str(board[i][j]))
+                self.guiBoard.add_widget(image)
+                self.fields[i][j] = image
+        self.mainGrid.add_widget(self.guiBoard)
+    def updateMap(self):
+        self.board=self.game.getBoard()
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                self.fields[i][j].text=str(self.board[i][j])
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        self.game.move(keycode[1])
+        self.game.updateFiledToFill()
+        self.updateMap()
+        return True
+
+
+class MainApp(App):
+    def build(self):
+        from kivy.base import runTouchApp
+        return runTouchApp(Template())
+
+
+if __name__ == "__main__":
+    MainApp().run()
